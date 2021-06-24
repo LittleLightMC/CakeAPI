d98198e52b8d5566cf03ac742dfbc32d8377fe0e
DarcJC <me@darc.pro>
2021-06-25 07:24:27 +0800

    💢Fixed yaml encoding bug

--- src/main/kotlin/pro/darc/cake/module/locale/LocaleManager.kt
+++ src/main/kotlin/pro/darc/cake/module/locale/LocaleManager.kt
@@ -59,7 +59,7 @@ object LocaleManager {
         val addonInfo = AddonManager.getAddon(addonId) ?: return null
         val path = "lang/$language.yml"
         val stream = addonInfo.instance?.getResource(path) ?: return null
-        val reader = InputStreamReader(stream)
+        val reader = InputStreamReader(stream, "UTF-8")
         return YamlConfiguration.loadConfiguration(reader).asLocale().apply {
             cachedUnit[key] = this
         }
