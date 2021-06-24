6fa28680c9eb483b28624aa7b2ddddaa30d32b98
DarcJC <me@darc.pro>
2021-06-24 14:24:45 +0800

    💫Clean invaild test

--- src/test/kotlin/pro/darc/cake/LifeInjectTest.kt
+++ src/test/kotlin/pro/darc/cake/LifeInjectTest.kt
@@ -11,22 +11,22 @@ import kotlin.test.assertTrue
 internal class LifeInjectTest: TestBase() {
 
 
-    companion object {
-        private val loader = LifecycleLoader
-
-        private var flag1 = false
-
-        @JvmStatic
-        @LifeInject([LifeCycle.CakeTest])
-        fun for_test_inject() {
-            flag1 = true
-        }
-    }
-
-    @Test
-    fun `test load inner life cycle injector`() {
-        loader.runLifecycle(LifeCycle.CakeTest)
-        assertTrue("Failed to execute test inject function") { flag1 }
-    }
+//    companion object {
+//        private val loader = LifecycleLoader
+//
+//        private var flag1 = false
+//
+//        @JvmStatic
+//        @LifeInject([LifeCycle.CakeTest])
+//        fun for_test_inject() {
+//            flag1 = true
+//        }
+//    }
+//
+//    @Test
+//    fun `test load inner life cycle injector`() {
+//        loader.runLifecycle(LifeCycle.CakeTest)
+//        assertTrue("Failed to execute test inject function") { flag1 }
+//    }
 
 }
